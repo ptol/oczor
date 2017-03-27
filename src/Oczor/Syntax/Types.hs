1939923d22a2fccd9b77abe0965b7264349b6028
Denis Stoyanov <stoyanov.gr@gmail.com>
2017-03-27 17:38:26 +0300

    Refactor - Recursion schemes for Oczor.Syntax.Types

--- src/Oczor/Syntax/Types.hs
+++ src/Oczor/Syntax/Types.hs
@@ -1,30 +1,33 @@
-{-# LANGUAGE PatternSynonyms       #-}
+{-# LANGUAGE TemplateHaskell, TypeFamilies #-}
+
 module Oczor.Syntax.Types where
 
-import Data.Functor.Foldable hiding (Foldable)
-import Oczor.Syntax.TypesF
 import ClassyPrelude hiding (TVar)
+import Data.Functor.Foldable.TH
+
+data TypeExpr =
+  TypeIdent String |
+  TypeVar String |
+  TypeUnion [TypeExpr] |
+  TypePoly [TypeExpr] TypeExpr |
+  TypeConstraints [(TypeExpr, String)] TypeExpr |
+  TypeFunc TypeExpr TypeExpr |
+  TypeLabel String TypeExpr |
+  TypeApply TypeExpr [TypeExpr] |
+  NoType |
+  TypeRecord [TypeExpr] |
+  TypeRow TypeExpr [TypeExpr]
+  deriving (Eq, Ord, Show, Read)
+
+makeBaseFunctor ''TypeExpr
 
 typeBool  = TypeIdent "Bool"
 typeUnit  = TypeIdent "Unit"
 typeArray x =  TypeApply (TypeIdent "Array") [x]
 
-type TypeExpr = Fix TypeExprF
+type ClassName = String
+type ConstraintSet = [(TypeExpr, ClassName)]
 
 type TVar = String
 data Scheme = Forall [TVar] TypeExpr
   deriving (Show, Eq, Ord, Read)
-
-type ConstraintSet = [(TypeExpr, ClassName)]
-
-pattern TypeUnion x = Fix (TypeUnionF x)
-pattern TypeIdent x = Fix (TypeIdentF x)
-pattern TypePoly x y = Fix (TypePolyF x y)
-pattern TypeVar x = Fix (TypeVarF x)
-pattern TypeConstraints x y = Fix (TypeConstraintsF x y)
-pattern TypeFunc x y = Fix (TypeFuncF x y)
-pattern TypeLabel x y = Fix (TypeLabelF x y)
-pattern TypeApply x y = Fix (TypeApplyF x y)
-pattern NoType  = Fix NoTypeF 
-pattern TypeRecord x = Fix (TypeRecordF x)
-pattern TypeRow x y = Fix (TypeRowF x y)
