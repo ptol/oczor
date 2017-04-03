b901fa80d22c90cad79e9d398b7b3685f0f096b1
Denis Stoyanov <stoyanov.gr@gmail.com>
2017-04-03 05:32:29 +0300

    Refactor - many minor changes (scopeToFunc (ScopeF [] y) = y)

--- src/Oczor/Converter/CodeGenAst.hs
+++ src/Oczor/Converter/CodeGenAst.hs
@@ -49,9 +49,8 @@ data Ast =
 
 makeBaseFunctor ''Ast
 
-scopeToFunc (ScopeF x y) = case x of
-  [] -> y
-  _ -> CallF (Parens (Function [] (embed <$> x <> [ReturnF (embed y)]))) []
+scopeToFunc (ScopeF [] y) = y
+scopeToFunc (ScopeF x y) = CallF (Parens (Function [] (embed <$> x <> [ReturnF (embed y)]))) []
   
 -- pattern Scope x <- Function _ x
 
