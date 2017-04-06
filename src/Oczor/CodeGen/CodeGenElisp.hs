96fd13ff2a44e61a5b9a931d0264365f3eb89167
Denis Stoyanov <stoyanov.gr@gmail.com>
2017-04-06 08:39:53 +0300

    Refactoring of CodeGenElisp (#31)
    * Refactor - many minor changes
    
    * Refactor - many minor changes (choice)
    
    * Refactor - CodeGenElisp (phi for codeGen)
    
    * Refactor - CodeGenElisp (refactoring of `scope` and `func`)
    
    * Refactor - CodeGenElisp (refactoring of `scope` and `func`)

--- src/Oczor/CodeGen/CodeGenElisp.hs
+++ src/Oczor/CodeGen/CodeGenElisp.hs
@@ -2,57 +2,57 @@ module Oczor.CodeGen.CodeGenElisp (codeGen) where
 import Oczor.CodeGen.Utl
 import qualified Prelude as P
 import Data.List.Split
+import Data.Functor.Foldable
+
+phi :: AstF Doc -> Doc
+phi = \case
+  NoneF -> empty
+  UniqObjectF name -> parens (text "lambda")
+  NotEqualF x y -> c "not" [c "eq" [x, y]]
+  EqualF x y -> c "eq" [x, y]
+  LitF value -> lit value
+  IdentF name -> ident name
+  LabelF x y -> p [text x, y]
+  VarF name ast -> c "setq" [ident name, ast]
+  SetF astl astr -> c "setq" [astl, astr]
+  ThrowF error -> c "error" [dquotes $ text error]
+  IfF p l r -> cn "if" [p, progn l, progn r]
+  ReturnF ast -> ast
+  FieldF ast name -> field ast name
+  HasFieldF ast name -> c "gethash" [symbol name, ast]
+  ObjectF list -> c "oc-hash-from-alist" [if onull list then text "'()" else cn "list" (list &map (\(name,ast) -> symbol name <+> ast))]
+  CallF name args -> c "funcall" (name : args)
+  OperatorF name x -> p (text name : x)
+  ArrayF list -> brackets (sep list)
+  ConditionOperatorF astb astl astr -> c "if" [astb,astl,astr]
+  BoolAndsF list -> c "and" list
+  StmtListF list -> progn list
+  ParensF x -> x
+  x -> error $ unwords ["codegen", show x]
 
 codeGen :: Ast -> Doc
 codeGen = code where
   code = \case
-    None -> empty
-    UniqObject name -> parens (text "lambda")
-    NotEqual x y -> c "not" [c "eq" [code x, code y]]
-    Equal x y -> c "eq" [code x, code y]
-    Lit value -> lit value
-    Ident name -> ident name
-    Label x y -> p [text x, code y]
-    Var name ast -> c "setq" [ident name, code ast]
     Set (Field ast name) astr -> c "puthash" [symbol name, code astr, code ast]
-    Set astl astr -> c "setq" [code astl, code astr]
-    Throw error -> c "error" [dquotes $ text error]
-    If p l r -> cn "if" [code p, progn (l <&> code), progn (r <&> code)]
-    Return ast -> code ast
-    Field ast name -> field (code ast) name
-    HasField ast name -> c "gethash" [symbol name, code ast]
-    Object list -> c "oc-hash-from-alist" [if onull list then text "'()" else cn "list" (list &map (\(name,ast) -> symbol name <+> code ast))]
     Function params body -> func params body
     Scope list y -> scope list y
-    Call name args -> c "funcall"((name : args) <&> code)
-    Operator name x -> p (text name : (x <&> code) )
-    Array list -> brackets (sep (list <&> code))
-    ConditionOperator astb astl astr -> c "if" [code astb,code astl,code astr]
-    BoolAnds list -> c "and" (list <&> code)
-    StmtList list -> progn (list <&> code)
-    Parens x -> code x
-    x -> error $ unwords ["codegen", show x]
+    x -> phi $ fmap codeGen $ project x
 
-scope list r  = bodyCode
+scope :: [Ast] -> Ast -> Doc
+scope list r  = bodyCode vars ffiVars
      where
-       body = list ++ [Return r]
-       (newBody, ffiVars) = fromMaybe (body, "") $ do
-         h <- headMay body
-         case h of {Code x -> Just (P.tail body, x); _ -> Nothing}
-       vars = newBody <&> getVarName & catMaybes <&> (\x -> p [ident x, nil])
-       bodyCode =
-         if onull vars && ffiVars == "" then progn (newBody <&> codeGen)
-         else cn "let*" $ parens (vcat (text ffiVars : vars)) : (newBody <&> codeGen)
+       (newBody, ffiVars) = case list ++ [Return r] of (Code x : t) -> (t, x); body -> (body, "")
+       vars = fmap (\x -> p [ident x, nil]) $ mapMaybe getVarName newBody
+       bodyCode [] "" = progn $ fmap codeGen newBody
+       bodyCode vars ffiVars = cn "let*" $ parens (vcat $ text ffiVars : vars) : fmap codeGen newBody
 
-func params body = p [text "lambda", p (params <&> ident), bodyCode ]
+func :: [String] -> [Ast] -> Doc
+func params body = p [text "lambda", p (fmap ident params), bodyCode vars ffiVars]
      where
-       (newBody, ffiVars) = fromMaybe (body, "") $ do
-         h <- headMay body
-         case h of {Code x -> Just (P.tail body, x); _ -> Nothing}
-       vars = newBody <&> getVarName & catMaybes <&> (\x -> p [ident x, nil])
-       bodyCode =
-         if onull vars && ffiVars == "" then progn (newBody <&> codeGen)
-         else c "let*" [parensNest (text ffiVars : vars ), progn (newBody <&> codeGen)]
+       (newBody, ffiVars) = case body of (Code x : t) -> (t, x); _ -> (body, "")
+       vars = fmap (\x -> p [ident x, nil]) $ mapMaybe getVarName newBody
+       bodyCode [] "" = progn $ fmap codeGen newBody
+       bodyCode vars ffiVars = c "let*" [parensNest $ text ffiVars : vars, progn $ fmap codeGen newBody]
 
 nil = text "nil"
 
